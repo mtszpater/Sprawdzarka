087e62b0fdd38e8e56d444c1f259ecfcb457fa56
KamilWro <kamil.breczko@gmail.com>
2017-10-29 11:42:21 +0100

    initial server module

---
+++ server/src/main/java/uwr/onlinejudge/server/ServerApplication.java
@@ -0,0 +1,11 @@
+package uwr.onlinejudge.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
