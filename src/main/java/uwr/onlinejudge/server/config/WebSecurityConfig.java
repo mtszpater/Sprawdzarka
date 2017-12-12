95403f460d6598b789f20701934d0fe2ecdb5325
KamilWro <kamil.breczko@gmail.com>
2017-12-12 23:28:40 +0100

    add logout option

--- src/main/java/uwr/onlinejudge/server/config/WebSecurityConfig.java
+++ src/main/java/uwr/onlinejudge/server/config/WebSecurityConfig.java
@@ -2,23 +2,14 @@ package uwr.onlinejudge.server.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import uwr.onlinejudge.server.models.User;
-
-import java.util.ArrayList;
 
 
 @Configuration
@@ -44,9 +35,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
+                .logoutUrl("/logout")
                 .permitAll();
         http.csrf().disable();
-
     }
 
     @Autowired
