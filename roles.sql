e9d7b9f0a81467493f5db72f9fe7c300db881d86
mtszpater <mtszpater@gmail.com>
2017-12-28 17:32:55 +0100

    roles spring security

---
+++ roles.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM wigo.role
+LIMIT 0, 1000
+
+-- Date: 2017-12-28 17:29
+*/
+INSERT INTO `role` (`id`,`name`) VALUES (0,'ROLE_ADMIN');
+INSERT INTO `role` (`id`,`name`) VALUES (1,'ROLE_USER');
