4fd5720eb6111bef90c15e9b2786aac68331a79f
KamilWro <kamil.breczko@gmail.com>
2018-01-02 20:37:41 +0100

    add group id in list form

--- src/main/java/uwr/onlinejudge/server/ServerApplication.java
+++ src/main/java/uwr/onlinejudge/server/ServerApplication.java
@@ -2,17 +2,10 @@ package uwr.onlinejudge.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class ServerApplication  extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ServerApplication.class);
-    }
-
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
