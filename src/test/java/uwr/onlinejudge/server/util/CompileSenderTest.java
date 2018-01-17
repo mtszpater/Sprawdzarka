fb85b1576f348834d97f1eb9a42071cbe2bbab0e
mtszpater <mtszpater@gmail.com>
2018-01-17 18:00:05 +0100

    correct test

--- src/test/java/uwr/onlinejudge/server/util/CompileSenderTest.java
+++ src/test/java/uwr/onlinejudge/server/util/CompileSenderTest.java
@@ -1,11 +1,7 @@
 package uwr.onlinejudge.server.util;
 
+import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 import uwr.onlinejudge.server.models.CodeToCompile;
 import uwr.onlinejudge.server.models.CompileResult;
 
@@ -14,7 +10,8 @@ public class CompileSenderTest {
     @Test
     public void Test1(){
         CompileSender compileSender = new CompileSenderImpl();
-        CodeToCompile codeToCompile = new CodeToCompile("0", "print('Hello2')", "");
-        System.out.println(compileSender.send(codeToCompile).getOutput());
+        CodeToCompile codeToCompile = new CodeToCompile("0", "print('Hello')", "");
+        CompileResult compileResult = compileSender.send(codeToCompile);
+        Assert.assertEquals(compileResult.getOutput().compareTo("Hello"), 1);
     }
 }
