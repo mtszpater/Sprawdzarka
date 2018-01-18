a541285589723177450a9a061ae6616663009b67
mtszpater <mtszpater@gmail.com>
2018-01-18 16:34:14 +0100

    inject services by constructor in userservice

--- src/main/java/uwr/onlinejudge/server/services/UserServiceImpl.java
+++ src/main/java/uwr/onlinejudge/server/services/UserServiceImpl.java
@@ -3,34 +3,33 @@ package uwr.onlinejudge.server.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import uwr.onlinejudge.server.models.Role;
 import uwr.onlinejudge.server.models.User;
 import uwr.onlinejudge.server.models.form.UserForm;
 import uwr.onlinejudge.server.repositories.RoleRepository;
 import uwr.onlinejudge.server.repositories.UserRepository;
 
-import java.util.Arrays;
 import java.util.HashSet;
 
 @Service
 public class UserServiceImpl implements UserService {
-    @Autowired
     private UserRepository userRepository;
-    @Autowired
     private RoleRepository roleRepository;
-    @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
-    @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder, ObjectMapper objectMapper) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.objectMapper = objectMapper;
+    }
 
     @Override
     public void save(UserForm userForm) {
         userForm.setPassword(bCryptPasswordEncoder.encode(userForm.getPassword()));
-
         User user = objectMapper.convertValue(userForm, User.class);
-
 
         user.setRoles(new HashSet<>(roleRepository.findAll()));
         userRepository.save(user);
