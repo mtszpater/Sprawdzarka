c075022dcb2d4db868a671f667f28ea6945ca6a7
mtszpater <mtszpater@gmail.com>
2018-01-19 17:49:36 +0100

    add TLE state in answerchecker

--- src/main/java/uwr/onlinejudge/server/util/AnswerCheckerImpl.java
+++ src/main/java/uwr/onlinejudge/server/util/AnswerCheckerImpl.java
@@ -10,11 +10,20 @@ public class AnswerCheckerImpl implements AnswerChecker {
     @Override
     public TestState check(CompileResult compileResult, Test test) {
         if (compileResult.getErrors().isEmpty()) {
+
+            String[] timeSplit = compileResult.getTime().split("\\.");
+            int executionTime = Integer.parseInt(timeSplit[1].replace("\n", ""));
+
+            if (executionTime > test.getTimeRequired()) {
+                return TestState.TLE;
+            }
+
             if (compileResult.getOutput().replace("\n", "").compareTo(test.getExpectedAnswer()) == 0) {
                 return TestState.OK;
             } else {
                 return TestState.WA;
             }
+
         } else {
             return TestState.RE;
         }
