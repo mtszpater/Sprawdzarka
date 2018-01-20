3ad5b3cae8ddf0cdbd97ca2e9cd1e22b805d7e92
mtszpater <mtszpater@gmail.com>
2018-01-20 15:01:40 +0100

    add ms to time and badgeclass around languages

--- src/main/java/uwr/onlinejudge/server/util/Languages.java
+++ src/main/java/uwr/onlinejudge/server/util/Languages.java
@@ -1,16 +1,18 @@
 package uwr.onlinejudge.server.util;
 
 public enum Languages {
-    PYTHON("python", 0),
-    C("C/C++", 7),
-    JAVA("java", 8);
+    PYTHON("python", 0, "badge-warning"),
+    C("C/C++", 7, "badge-dark"),
+    JAVA("java", 8, "badge-danger");
 
     private String name;
     private int id;
+    private String badgeClass;
 
-    Languages(String name, int id) {
+    Languages(String name, int id, String badgeClass) {
         this.name = name;
         this.id = id;
+        this.badgeClass = badgeClass;
     }
 
     public int getId() {
@@ -19,5 +21,9 @@ public enum Languages {
 
     public String getName() {
         return name;
+    }
+
+    public String getBadgeClass() {
+        return badgeClass;
     }
 }
