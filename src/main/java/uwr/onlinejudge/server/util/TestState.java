27d011e40b41b4daa98e107e80197f61336c7d86
mtszpater <mtszpater@gmail.com>
2018-01-20 17:23:31 +0100

    refactoring

--- src/main/java/uwr/onlinejudge/server/util/TestState.java
+++ src/main/java/uwr/onlinejudge/server/util/TestState.java
@@ -1,25 +1,30 @@
 package uwr.onlinejudge.server.util;
 
 public enum TestState {
-    TLE("Timelimit", 0),
-    RE("Runtime error", 1),
-    WA("Wrong answer", 2),
-    OK("Correct answer", 3);
+    TLE("Timelimit", 0, "text-info"),
+    RE("Runtime error", 1, "text-warning"),
+    WA("Wrong answer", 2, "text-danger"),
+    OK("Correct answer", 3, "text-success");
 
-    private int id;
     private String title;
+    private int id;
+    private String textClass;
 
-    TestState(String title, int id) {
+    TestState(String title, int id, String textClass) {
         this.title = title;
         this.id = id;
-    }
-
-    public int getId() {
-        return id;
+        this.textClass = textClass;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getTextClass() {
+        return textClass;
+    }
 }
