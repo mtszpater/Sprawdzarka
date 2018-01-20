ad470804a679d04be3403b1d4b38a4fa398dd2a8
mtszpater <mtszpater@gmail.com>
2018-01-20 23:53:43 +0100

    refactoring

--- src/main/java/uwr/onlinejudge/server/util/TestType.java
+++ src/main/java/uwr/onlinejudge/server/util/TestType.java
@@ -1,16 +1,18 @@
 package uwr.onlinejudge.server.util;
 
 public enum TestType {
-    PUBLIC("publiczny test", 0),
-    PRIVATE("prywatny test", 1),
-    HIDDEN("ukryty test", 2);
+    PUBLIC("publiczny test", 0, "badge-primary"),
+    PRIVATE("prywatny test", 1, "badge-secondary"),
+    HIDDEN("ukryty test", 2, "badge-dark");
 
     private String title;
     private int id;
+    private String badgeClass;
 
-    TestType(String title, int id) {
+    TestType(String title, int id, String badgeClass) {
         this.title = title;
         this.id = id;
+        this.badgeClass = badgeClass;
     }
 
     public int getId() {
@@ -21,4 +23,7 @@ public enum TestType {
         return title;
     }
 
+    public String getBadgeClass() {
+        return badgeClass;
+    }
 }
